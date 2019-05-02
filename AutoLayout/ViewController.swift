8c022a7be33b8836528e81504b760b7342ee1983
patelpra337 <patelpra337@gmail.com>
2019-05-01 21:03:51 -0500

    New AutoLayout Project

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by patelpra on 5/1/19.
+//  Copyright © 2019 Pravin Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
